ba5b58ccb9b10f154b3d60c8e2bfaf8d5358e3e4
zhanglei_zl <zhanglei_zl@youzan.com>
2025-05-29 17:09:01 +0800

    feat: 用了阿木木favicon

--- packages/amumu-frontend/app/layout.tsx
+++ packages/amumu-frontend/app/layout.tsx
@@ -1,34 +1,41 @@
 // import './globals.css';
 
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import {
+  Geist,
+  Geist_Mono,
+} from 'next/font/google';
 
 const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin']
-});
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+})
 
 const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin']
-});
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+})
 
 export const metadata: Metadata = {
-	title: 'Briar',
-	description: '',
-	icons: {
-		icon: 'https://static.stardew.site/apple-touch-icon.png'
-	}
-};
+  title: "Briar",
+  description: "",
+  icons: {
+    icon: "https://zzcjbauazgbnnpxahajj.supabase.co/storage/v1/object/public/amumu/img/bitbug_favicon.ico",
+  },
+}
 
 export default function RootLayout({
-	children
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-	return (
-		<html lang="en">
-			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  )
 }
